1c7ae6881d93fcb67f18945b0a1a5c3d73f6bf56
Raha <erbees.50000@gmail.com>
2021-03-11 13:19:44 +0700

    martriks array

---
+++ src/array.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 4; i++ {
+		for j := 1; j <= 5; j++ {
+			if i == j {
+				fmt.Print(0)
+			} else {
+				fmt.Print(1)
+			}
+		}
+		fmt.Println()
+	}
+}
