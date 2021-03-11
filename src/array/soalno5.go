cc10d59b93473e50f7b6ed2d219f292358611f97
Raha <erbees.50000@gmail.com>
2021-03-11 14:04:24 +0700

    soal 4-5

---
+++ src/array/soalno5.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	bilangan := 1
+	for i := 1; i <= 4; i++ {
+		for j := 1; j <= 4; j++ {
+			if i >= j {
+				fmt.Print(bilangan)
+			} else {
+				fmt.Print(0)
+			}
+		}
+		bilangan = bilangan + 1
+		fmt.Println()
+	}
+}
